5c1a2eea1cf7e0270822e2c737e99f8e495cc06c
Hugo Bender <benderhugo@hotmail.com>
2018-08-23 20:44:27 +0200

    commit Step002

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,24 @@
+//--Modulos en angular
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { AppRoutingModule } from './app-routing.module';
+//--Material
+import { AppMaterialModule } from './components/app-material/app-material.module';
+//--componentes
 import { AppComponent } from './app.component';
+import { AppToolBarComponent } from './components/app-tool-bar/app-tool-bar.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AppToolBarComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    BrowserAnimationsModule,
+    AppRoutingModule,
+    AppMaterialModule
   ],
   providers: [],
   bootstrap: [AppComponent]
