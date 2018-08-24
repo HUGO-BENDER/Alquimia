84a1df8c35315d9a5f855e3b46087decb74161e6
Hugo Bender <benderhugo@hotmail.com>
2018-08-25 00:34:51 +0200

    commit step004 -add routes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,36 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { PageHomeComponent } from './components/page-home/page-home.component';
+import { PageAboutComponent } from './components/page-about/page-about.component';
+import { PageNotFoundComponent } from './components/page-not-found/page-not-found.component';
+import { PageGameComponent } from './components/page-game/page-game.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    component: PageHomeComponent
+  }, {
+    path: 'home',
+    component: PageHomeComponent
+  },
+  {
+    path: 'game',
+    component: PageGameComponent
+  },
+  {
+    path: 'about',
+    component: PageAboutComponent
+  }, {
+    path: '**',
+    component: PageNotFoundComponent
+  }
+];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [RouterModule.forRoot(routes,
+    {
+      enableTracing: true
+    })],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
