583952c4363c32139002125a6e5ec9c7f6cce1b4
Hugo Bender <benderhugo@hotmail.com>
2018-11-04 22:44:53 +0100

    commit009 domingo noche

--- functions/lib/index.js
+++ functions/lib/index.js
@@ -33,87 +33,117 @@ exports.OnAddNewGame = functions.firestore
         const FieldValue = require('firebase-admin').firestore.FieldValue;
         const pathGame = '/Games/' + context.params.gameId;
         const newGame = snap.data();
-        // // -- Fake for GetSettingGame.  2 to 4 players. 
-        // // -- For development only 2 players.
-        // const configGame = {
-        //     players: {
-        //         cant: 2,
-        //         hand: 5
-        //     },
-        //     cards: {
-        //         valueCards: [1, 2, 3, 4, 5, 6, 7, 8, 9],
-        //         descCards: ['1', '2', '3', '4', '5', '6', '7', '8', '9'],
-        //         suitsCards: ['ESP', 'ORO', 'COP', 'BAS', 'TRE', 'COR', 'DIA', 'PIC']
-        //     },
-        //     board: {
-        //         cols: 9,
-        //         rows: 4
-        //     }
-        // }
-        // // -- Variable Game actual
-        // const CurrentGame = {
-        //     Baraja : [],
-        //     Players: [
-        //         {
-        //             id: '',
-        //             hand: []
-        //         }
-        //     ],
-        //     Board: [{
-        //         Id: 0,
-        //         IdUserWin: '',
-        //         NameUserWin: '',
-        //         Target: {
-        //             Id: 0,
-        //             dropEnable: false,
-        //             classCss: 'planet '
-        //         },
-        //         Players: [{
-        //             Id: '',
-        //             Name: '',
-        //             Displayed: []
-        //         }]
-        //     }],
-        //     DisplayedCard: {}
-        // };
-        // // -- Create collection deck of cards in temporal array
-        // let cont = 1;
-        // for (const s of configGame.cards.suitsCards) {
-        //     for (const v of configGame.cards.valueCards) {
-        //         CurrentGame.Baraja.push({
-        //             id: cont,
-        //             palo: s,
-        //             valor: v,
-        //             index: 0,
-        //             dragEnable: false,
-        //             classCss: 'card '
-        //         });
-        //         cont += 1;
-        //     }
-        // }
-        // // -- shuffle
-        // for (let i = CurrentGame.Baraja.length - 1; i >= 0; i--) {
-        //     const randomIndex = Math.floor(Math.random() * (i + 1));
-        //     const itemAtIndex = CurrentGame.Baraja[randomIndex];
-        //     CurrentGame.Baraja[randomIndex] = CurrentGame.Baraja[i];
-        //     CurrentGame.Baraja[i] = itemAtIndex;
-        // }
-        // // -- players hands
-        // let indexLast = CurrentGame.Baraja.length - 1;
-        // const totalForPlayers = configGame.players.cant * configGame.players.hand;
-        // for (let i = 0; i < totalForPlayers; i++) {
-        //     const indexPlayer = (i + configGame.players.cant) % configGame.players.cant;
-        //     CurrentGame.Players[indexPlayer].hand.push(CurrentGame.Baraja[indexLast]);
-        //     CurrentGame.Players[indexPlayer].hand[CurrentGame.Players[indexPlayer].hand.length - 1].dragEnable = true;
-        //     CurrentGame.Players[indexPlayer].hand[CurrentGame.Players[indexPlayer].hand.length - 1].index = CurrentGame.Players[indexPlayer].hand.length;
-        //     indexLast--;
-        //     CurrentGame.Baraja.pop();
-        // }
-        // // -- Card on table
-        // CurrentGame.DisplayedCard = CurrentGame.Baraja[indexLast];
-        // CurrentGame.Baraja.pop();
+        console.log('podemos leer newGame.gameType : ', newGame.gameType);
+        // -- Fake for GetSettingGame.  2 to 4 players. 
+        // -- For development only 2 players.
+        const ConfigGame = {
+            Players: {
+                cant: 2,
+                hand: 5
+            },
+            Cards: {
+                valueCards: [1, 2, 3, 4, 5, 6, 7, 8, 9],
+                descCards: ['1', '2', '3', '4', '5', '6', '7', '8', '9'],
+                suitsCards: ['ESPADA', 'ORO', 'COPA', 'BASTO', 'TREBOL', 'CORAZON', 'DIAMANTE', 'PICA']
+            },
+            board: {
+                cols: 9,
+                rows: 4
+            }
+        };
+        console.log('ConfigGame');
+        // -- Variable Game actual
+        const CurrentGame = {
+            Baraja: [{
+                    id: 0,
+                    position: 0,
+                    palo: '',
+                    valor: 0,
+                    description: '',
+                    dragEnable: false,
+                    classCss: 'card'
+                }],
+            Players: [
+                {
+                    id: '',
+                    hand: []
+                }
+            ],
+            Board: [{
+                    Id: 0,
+                    IdUserWin: '',
+                    NameUserWin: '',
+                    Target: {
+                        id: 0,
+                        dropEnable: false,
+                        classCss: 'goal'
+                    },
+                    Players: [{
+                            Id: '',
+                            Name: '',
+                            Displayed: []
+                        }]
+                }],
+            DisplayedCard: {
+                id: 0,
+                position: 0,
+                palo: '',
+                valor: 0,
+                description: '',
+                dragEnable: false,
+                classCss: 'card'
+            }
+        };
+        console.log('CurrentGame');
+        // -- Create collection deck of cards in temporal array
+        let cont = 1;
+        CurrentGame.Baraja = [];
+        for (const s of ConfigGame.Cards.suitsCards) {
+            let contValue = 0;
+            for (const v of ConfigGame.Cards.valueCards) {
+                CurrentGame.Baraja.push({
+                    id: cont,
+                    position: cont,
+                    palo: s,
+                    valor: v,
+                    description: ConfigGame.Cards.descCards[contValue],
+                    dragEnable: false,
+                    classCss: 'card '
+                });
+                cont += 1;
+                contValue += 1;
+            }
+        }
+        console.log('creamos la baraja con ', CurrentGame.Baraja.length, ' cartas');
+        // -- shuffle
+        for (let i = CurrentGame.Baraja.length - 1; i >= 0; i--) {
+            const randomIndex = Math.floor(Math.random() * (i + 1));
+            CurrentGame.Baraja[randomIndex].position = i;
+            CurrentGame.Baraja[i].position = randomIndex;
+        }
+        console.log('mezclamos');
+        // -- players hands
+        const totalForPlayers = ConfigGame.Players.cant * ConfigGame.Players.hand;
+        console.log('totalForPlayers', totalForPlayers);
+        for (let i = 0; i < ConfigGame.Players.cant; i++) {
+            CurrentGame.Players.push({
+                id: '_' + i,
+                hand: [],
+            });
+        }
+        console.log('creamos el array de jugadores');
+        for (let i = 0; i < totalForPlayers; i++) {
+            const indexPlayer = (i + ConfigGame.Players.cant) % ConfigGame.Players.cant;
+            CurrentGame.Players[indexPlayer].hand.push(CurrentGame.Baraja.pop());
+            CurrentGame.Players[indexPlayer].hand[CurrentGame.Players[indexPlayer].hand.length - 1].dragEnable = true;
+            CurrentGame.Players[indexPlayer].hand[CurrentGame.Players[indexPlayer].hand.length - 1].index = CurrentGame.Players[indexPlayer].hand.length;
+        }
+        console.log('cargamos la mano de los jugadores');
+        // -- Card on table
+        CurrentGame.DisplayedCard = CurrentGame.Baraja.pop();
+        console.log('pusimos la carta en la mesa');
         // // -- Boardgame
-        // for (let x = 0; x < configGame.board.cols; x++) {
+        // for (let x = 0; x < ConfigGame.board.cols; x++) {
         //     CurrentGame.Board.push({
         //         Id: x,
         //         IdUserWin: '',
@@ -139,10 +169,44 @@ exports.OnAddNewGame = functions.firestore
         console.log('newGame.gameType es ', newGame.gameType);
         snap.ref.set({
             timeStart: FieldValue.serverTimestamp(),
-        }, {
-            merge: true
-        });
+            displayedCard: {
+                id: CurrentGame.DisplayedCard.id,
+                position: CurrentGame.DisplayedCard.position,
+                palo: CurrentGame.DisplayedCard.palo,
+                valor: CurrentGame.DisplayedCard.valor,
+                dragEnable: true,
+                classCss: 'card displayed onTable'
+            }
+        }, { merge: true });
         console.log('pasamos el  snap.ref.set');
+        console.log('vamos a buclear la baraja de ', CurrentGame.Baraja.length);
+        //  for (const c of CurrentGame.Baraja) {
+        //     console.log(c.position.toString(),c.description,'_de_',c.palo );
+        // }
+        // console.log('pasamos el  for de pruebas');
+        for (const c of CurrentGame.Baraja) {
+            const k = ('0' + c.position.toString()).slice(-2);
+            console.log('la clave es ', k);
+            const r = yield fdb.doc(pathGame).collection('Baraja').doc(k).set({
+                id: c.id,
+                position: c.position,
+                palo: c.palo,
+                valor: c.valor,
+                description: c.description,
+                dragEnable: false,
+                classCss: 'card'
+            });
+            // const r = await fdb.doc(pathGame).collection('Baraja').doc(c.description + '-'+ c.palo).set({
+            //     id: c.id,
+            //     position: c.id,
+            //     palo: c.palo,
+            //     valor: c.valor,
+            //     description: c.description,
+            //     dragEnable: false,
+            //     classCss: 'card'
+            // });
+        }
+        console.log('snap.ref.collection(Baraja).doc');
         return Promise.resolve('essssssaaaaaa');
     }
     catch (err) {
