0b2a3a2fa813b74987ec6f45a59316e54b121f9d
Hugo Bender <benderhugo@hotmail.com>
2018-11-07 17:51:55 +0100

    commit010_002

--- src/app/components/page-home/page-home.component.ts
+++ src/app/components/page-home/page-home.component.ts
@@ -2,10 +2,11 @@ import { Component, OnInit } from '@angular/core';
 import { MatSnackBar, MatSnackBarVerticalPosition } from '@angular/material';
 import { AngularFireAuth } from 'angularfire2/auth';
 import { Observable } from 'rxjs/Observable';
-import { RecruitmentService } from '../../services/firestore/Recruitment.service';
-import { Recruitment, recruitmentState } from '../../model/recruitment';
-import { MinInfoPlayer } from '../../model/player';
-import { GameInProgress } from '../../model/game';
+import { RecruitmentService } from 'src/app/services/firestore/Recruitment.service';
+import { PlayerService } from 'src/app/services/firestore/player.service';
+import { Recruitment, recruitmentState } from 'src/app/model/recruitment';
+import { MinInfoPlayer } from 'src/app/model/player';
+import { GameInProgress } from 'src/app/model/game';
 import * as firebase from 'firebase';
 
 @Component({
@@ -27,15 +28,26 @@ export class PageHomeComponent implements OnInit {
   userlogined: firebase.User;
   snackBarVerticalPositionTop: MatSnackBarVerticalPosition = 'top';
 
-  constructor(public au: AngularFireAuth, private afsRecruitments: RecruitmentService, public snackBar: MatSnackBar) { }
+  constructor(public au: AngularFireAuth, private afsRecruitments: RecruitmentService,
+              public afsPlayer: PlayerService, public snackBar: MatSnackBar) { }
 
   ngOnInit() {
 
     this.au.authState.subscribe(user => {
       if (user) {
         this.userlogined = user;
+        this.gamesInProgress = this.afsPlayer.getGamesInProgress(user).map(
+          actions => {
+            return actions.map(action => {
+              const data = action.payload.doc.data() as GameInProgress;
+              const id = action.payload.doc.id;
+              return { id, ...data };
+            });
+          }
+        );
       } else {
         this.userlogined = null;
+        this.gamesInProgress = null;
       }
     });
 
